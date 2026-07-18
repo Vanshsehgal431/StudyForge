21ab6ae0261fa3441ad092d580b1c9603e57ec66
Vanshsehgal431 <vanshsehgal1212@gmail.com>
2026-07-18 22:40:24 +0530

    Debug API URL

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 const API_URL = import.meta.env.VITE_API_URL;
+console.log("API_URL =", API_URL);
 function Register() {
   const [fullName, setFullName] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
